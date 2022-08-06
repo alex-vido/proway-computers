5e54693c74ab7a44a2fea4d7eb2b0bf270164635
alex-vido <alex.vido93@gmail.com>
2022-08-06 15:51:47 -0300

    Atualização das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { ContatoComponent } from './contato/contato.component';
 
 const routes: Routes = [
-  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  { path: 'produtos', component: ProdutosComponent},
   { path: "", redirectTo: "produtos", pathMatch: "full"},
-  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
-  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
+  { path: 'carrinho', component: CarrinhoComponent},
+  { path: 'contato', component: ContatoComponent},
   { path: "**", component: NaoEncontradoComponent}
 ];
 
