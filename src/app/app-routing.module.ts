e339915ab1594605c5bf74dac098c60079770a78
alex-vido <alex.vido93@gmail.com>
2022-08-06 15:56:44 -0300

    Mudança na forma de roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
-import { ProdutosComponent } from './produtos/produtos.component';
-import { CarrinhoComponent } from './carrinho/carrinho.component';
-import { ContatoComponent } from './contato/contato.component';
+
 
 const routes: Routes = [
-  { path: 'produtos', component: ProdutosComponent},
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   { path: "", redirectTo: "produtos", pathMatch: "full"},
-  { path: 'carrinho', component: CarrinhoComponent},
-  { path: 'contato', component: ContatoComponent},
+  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
   { path: "**", component: NaoEncontradoComponent}
 ];
 
